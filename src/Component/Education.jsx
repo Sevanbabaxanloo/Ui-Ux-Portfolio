e0481556eb348a23fbc18b8bebbdd58a914319f7
Sevan Babaxanloo <sevan_babaxanloo@yahoo.com>
2024-12-01 01:11:19 +0400

    final

--- src/Component/Education.jsx
+++ src/Component/Education.jsx
@@ -9,10 +9,13 @@ const styles = {
     "text-[#FEFFFF] text-[36px] font-[600] max-md:text-[32px] max-xs:text-center",
   EducationChildrenDiv:
     "flex flex-col gap-[24px] max-md:items-center max-xs:w-full",
+  EducationChildrenDivChild: "max-xs:w-full",
   EducationButtonDiv:
     "flex flex-row bg-[var(--bg-box)] h-[53px] rounded-[24px] p-[10px] max-md:w-[341px] max-xs:w-full",
+  EducationButtonDivContainer: "w-full max-md:flex max-md:justify-center",
   EducationButtonDivP:
     "h-full bg-[var(--btn-purple)] shadow-custom rounded-[69px] text-[#FEFFFF] text-[14px] font-[600] px-[16px] py-[6px] max-md:w-full max-md:text-center",
+  EducationChildrenContainerMain: "flex flex-col gap-[12px] max-xs:w-full",
   EducationChildrenContainer:
     "h-[115px] bg-[var(--bg-box)] border-[1px] border-[#525252] rounded-[24px] p-[24px] max-md:w-[341px] max-xs:w-full max-xs:h-fit max-xs:flex max-xs:flex-col max-xs:gap-[4px]",
   EducationChildrenContainerH2:
@@ -26,28 +29,33 @@ const styles = {
 const Education = () => {
   const object = [
     {
-      text: "SoftConstruct",
-      paragraph: "Product Designer",
-      date: "2021 - Present (Full time)",
+      company: "DevSoft Armenia",
+      jobtitle: "UI/UX Designer",
+      date: "2024 - Present (Full time)",
     },
     {
-      text: "Happy Life",
-      paragraph: "UI/UX Designer",
+      company: "SoftConstruct",
+      jobtitle: "Product Designer",
+      date: "2021 - 2024 (Full time)",
+    },
+    {
+      company: "Happy Life",
+      jobtitle: "UI/UX Designer",
       date: "2022 - Present (Part time)",
     },
     {
-      text: "Futurest",
-      paragraph: "UI/UX Designer/ Stakeholder",
+      company: "Futurest",
+      jobtitle: "UI/UX Designer/ Stakeholder",
       date: "2021 - Present (Freelance)",
     },
     {
-      text: "E-World LLC",
-      paragraph: "UI/UX Designer",
+      company: "E-World LLC",
+      jobtitle: "UI/UX Designer",
       date: "6 months (Full time) (One time project)",
     },
     {
-      text: "ITHIRE LLC",
-      paragraph: "UI/UX Designer",
+      company: "ITHIRE LLC",
+      jobtitle: "UI/UX Designer",
       date: "2020 - 2021 (Full time)",
     },
   ];
@@ -55,24 +63,24 @@ const Education = () => {
     <div className={styles.EducationContainer}>
       <div className={styles.EducationTextDiv}>
         <p className={styles.EducationTextP}>Background section</p>
-        <h2 className={styles.EducationTextH2}>Education & Career</h2>
+        <h2 className={styles.EducationTextH2}>Career</h2>
       </div>
       <div className={styles.EducationChildrenDiv}>
-        <div className="max-xs:w-full">
+        <div className={styles.EducationChildrenDivChild}>
           <div className={styles.EducationButtonDiv}>
-            <div className="w-full max-md:flex max-md:justify-center">
+            <div className={styles.EducationButtonDivContainer}>
               <p className={styles.EducationButtonDivP}>Career</p>
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-[12px] max-xs:w-full">
+        <div className={styles.EducationChildrenContainerMain}>
           {object.map((item, index) => (
             <div key={index} className={styles.EducationChildrenContainer}>
               <h2 className={styles.EducationChildrenContainerH2}>
-                {item.text}
+                {item.company}
               </h2>
               <p className={styles.EducationChildrenContainerPOne}>
-                {item.paragraph}
+                {item.jobtitle}
               </p>
               <p className={styles.EducationChildrenContainerPTwo}>
                 {item.date}
